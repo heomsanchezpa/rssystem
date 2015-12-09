b5bd38e75f3d6bf7b8a4e702ae3c7fedd179dd7c
Gianny <giedriveraza@ittepic.edu.mx>
2015-12-08 22:33:23 -0700

    modificacion3

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=rssystemm',
+    'dsn' => 'mysql:host=localhost;dbname=rssystem',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
