bc189c469970ce105d7d60167e5d160a75967926
omarsantosma <omalsantosma@ittepic.edu.mx>
2015-12-08 22:18:07 -0700

    prueba modificar

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=rssystem',
+    'dsn' => 'mysql:host=localhost;dbname=rssyste',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
